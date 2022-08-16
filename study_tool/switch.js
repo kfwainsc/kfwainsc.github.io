1bf002f0a7fc1395ecc82612ae308c5e0452a64c
kfwainsc <kendrawainscott@gmail.com>
2022-08-16 12:38:40 -0700

    js working for coming soon quiz. started quiz-face

--- study_tool/switch.js
+++ study_tool/switch.js
@@ -1,24 +1,38 @@
-//alert("Hello");
+const myBody = document.getElementById("myBody");
+const changeBtn = document.getElementById("changeBtn");
+changeBtn.addEventListener(
+  "click",
+  (randBgColor = () => {
+    const colors = ["lightgrey", "grey", "orange", "lightblue", "darkgreen", "darkblue", "indigo"];
+    let num = Math.floor(Math.random() * 7);
+    if (num > 2) {
+      myBody.style.color = "whitesmoke";
+    } else myBody.style.color = "black";
+    myBody.style.backgroundColor = colors[num];
+  })
+);
 
-function giveTime() {
-  //const timeH2 = document.getElementById("time");
-  //timeH2.innterHTML = Date();
-  if (document.getElementById("time").innerHTML == "t") {
-    document.getElementById("time").innerHTML = Date();
-    document.getElementById("time").style.display = "block";
-    document.getElementById("timeBtn").innerHTML = "jk, dont want time";
-  } else {
-    document.getElementById("time").innerHTML = "t";
-    document.getElementById("time").style.display = "none";
-    document.getElementById("timeBtn").innerHTML = "give me Time";
-  }
-}
+const time = document.getElementById("time");
+const timeBtn = document.getElementById("timeBtn");
+timeBtn.addEventListener(
+  "click",
+  (giveTime = () => {
+    if (time.innerHTML === "t") {
+      time.innerHTML = Date();
+      time.style.display = "block";
+      timeBtn.innerHTML = "jk, dont want time";
+    } else {
+      time.innerHTML = "t";
+      time.style.display = "none";
+      timeBtn.innerHTML = "give me Time";
+    }
+  })
+);
 
-function randBgColor() {
-  const colors = ["lightgrey", "grey", "orange", "lightblue", "darkgreen", "darkblue", "indigo"];
-  let num = Math.floor(Math.random() * 7);
-  if (num > 2) {
-    document.getElementById("myBody").style.color = "whitesmoke";
-  } else document.getElementById("myBody").style.color = "black";
-  document.getElementById("myBody").style.backgroundColor = colors[num];
-}
+const emergencyBtn = document.getElementById("emergency");
+emergencyBtn.addEventListener(
+  "click",
+  (callAlert = () => {
+    alert("GET OFF THIS PAGE and call 9-1-1");
+  })
+);
