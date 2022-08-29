d0c24dcd1d3aee13fa3c6734f50f41d558536f29
kfwainsc <kendrawainscott@gmail.com>
2022-08-29 13:14:22 -0700

    fixed stupid spinning ovals

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Portfollio Projects Overview
 Avaible to try at https://kfwainsc.github.io/
 
 Showcase of various projects at different stages of development.
+All projects are screen size responsive and scale accordingly along with the addition or subtraction of content as needed to maintain a clean design and intuitive user experience.
--- landingScript.js
+++ landingScript.js
@@ -1,10 +1,10 @@
-let screenWidth = screen.width;
-
 // need imporvement
 // will not resize again if width changes
-if (screenWidth <= 480) {
-  const seeMore = document.getElementById("see-more");
-  seeMore.innerHTML = "to see more...";
+if (screen.width <= 480) {
+  document.querySelector("#see-more h2").innerHTML = "to see more...";
+}
+if (screen.width >= 650) {
+  document.getElementById("pump-img").src = "./images/pumpPanelWide.jpg";
 }
 //  if wide enough for squares, use large truck pic
 
